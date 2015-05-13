e91365bdedf2dcaf35b784223b2837da214fbcbc
zhanghengyang <danielzhang-1@163.com>
2015-05-13 19:21:48 +0800

    test the new relic but error log not responding

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,37 +1,55 @@
-from flask import Flask
 import rollbar
 import os
 import rollbar.contrib.flask
 
-ACCESS_TOKEN = "ca7f0172c3d44f54a17c75367116bd2a"
-ENVIRONMENT = "production"
-rollbar.init(ACCESS_TOKEN,ENVIRONMENT)
+#####new relic demo
+####
+#### Reference:
+#### https://docs.newrelic.com/docs/agents/python-agent/getting-started/python-agent-quick-start
+#
+# import newrelic.agent
+cur_path = os.path.abspath(".")
+#newrelic.agent.initialize(cur_path + "/newrelic.ini","staging")
+
+
+
+from flask import Flask
+
 app = Flask(__name__)
 
-from flask import got_request_exception
+##### rollbar demo
+##### Reference
+##### https://rollbar.com/docs/notifier/pyrollbar/
+
+# ACCESS_TOKEN = "ca7f0172c3d44f54a17c75367116bd2a"
+# ENVIRONMENT = "production"
+# rollbar.init(ACCESS_TOKEN,ENVIRONMENT)
+# from flask import got_request_exception
+#
+#
+# @app.before_first_request
+# def init_rollbar():
+#     """init rollbar module"""
+#     rollbar.init(
+#
+#         ACCESS_TOKEN,
+#         # environment name
+#         ENVIRONMENT,
+#         # server root directory, makes tracebacks prettier
+#         root=os.path.dirname(os.path.realpath(__file__)),
+#         # flask already sets up logging
+#         allow_logging_basic_config=False)
+#
+#     # send exceptions from `app` to rollbar, using flask's signal system.
+#     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
-
-        ACCESS_TOKEN,
-        # environment name
-        ENVIRONMENT,
-        # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
-        # flask already sets up logging
-        allow_logging_basic_config=False)
-
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
 @app.route('/')
 def hello_world():
 
-#try: if catch the exception.
+#try:
     raise Exception("what is the fukc");
 #except:
     print "there is an exception!"
@@ -41,6 +59,6 @@ def hello_world():
 
 if __name__ == '__main__':
     #report one message
-    rollbar.report_message("get one message","error");
+    #rollbar.report_message("get one message","error");
 
     app.run(port=9001)
